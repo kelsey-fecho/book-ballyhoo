f926875349f07e9c61890a380b7a4f07f1b4af0f
Kelsey Fecho <kfecho30@gmail.com>
2018-08-29 18:07:30 -0400

    Add strong params for q (query) functionality

--- app/controllers/book_controller.rb
+++ app/controllers/book_controller.rb
@@ -1,10 +1,17 @@
 class BookController < ApplicationController
-
   def index
-    render(
-      status: 200,
-      json: Book.all
-    )
+    q = params[:q]
+    if q.blank?
+      render status: 200, json: Book.where("list=?", "fiction")
+    else
+      render status: 200, json: Book.where("list=?", q)
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:q)
   end
 
 end
