0959f70390ad2c769ada7694a05f9c8b1c8d1adb
Kelsey Fecho <kfecho30@gmail.com>
2018-08-29 17:29:52 -0400

    Seed database with current New York Times data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,215 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Book.create({title: "Girls Burn Brighter", author: "Shobha Rao", desc: "A searing, electyifying debut novel set in India and America about the extraordinary bond between 2 girls driven apart by circumstances but relentless in their search for one another."})
-Book.create({title: "Bossypants", author: "Tina Fey", desc: "Loaded with personality, insights into power, and the kind of humor that can cause beverages to travel through the reader's nasal passages unplanned."})
+Book.create([
+  {
+    title: "Crazy Rich Asians",
+    author: "Kevin Kwan",
+    desc: "A New Yorker gets a surprise when she spends the summer with her boyfriend in Singapore.",
+    url:"https://www.amazon.com/Crazy-Rich-Asians-Trilogy/dp/0345803787?tag=NYTBS-20",
+    list: "ficton"
+  },
+  {
+    title: "Texas Ranger",
+    author: "James Patterson",
+    desc: "Rory Yates's job and reputation are at risk when he is accused of killing his ex-wife.",
+    url:"https://www.amazon.com/Texas-Ranger-James-Patterson-ebook/dp/B078LXGP4B?tag=NYTBS-20",
+    list: "fiction"
+  },
+  {
+    title: "Origin",
+    author: "Dan Brown",
+    desc: "A symbology professor goes on a perilous quest with a beautiful museum director.",
+    url: "https://www.amazon.com/Origin-Novel-Dan-Brown-ebook/dp/B01LY7FD0D?tag=NYTBS-20",
+    list: "fiction"
+  },
+  {
+    title: "Sharp Objects",
+    author: "Gillian Flynn",
+    desc: "After a stay at a psychiatric hospital, a reporter reluctantly returns to her hometown to cover the murders of two girls.",
+    url: "http://www.amazon.com/Sharp-Objects-Novel-Gillian-Flynn/dp/0307341550?tag=NYTBS-20",
+    list: "fiction"
+  },
+  {
+    title: "Feared",
+    author: "Lisa Scottoline",
+    desc: "Mary DiNunzio and Bennie Rosato must solve a murder and handle a reverse sex discrimination suit against their firm.",
+    url: "https://www.amazon.com/Feared-Rosato-DiNunzio-Lisa-Scottoline-ebook/dp/B0796XQBPY?tag=NYTBS-20",
+    list: "fiction"
+  },
+  {
+    title: "China Rich Girlfriend",
+    author: "Kevin Kwan",
+    desc: "The second book in the Crazy Rich Asians trilogy. As her wedding draws near, Rachel Chu discovers her birth father.",
+    url: "https://www.amazon.com/China-Girlfriend-Crazy-Asians-Trilogy/dp/0804172064?tag=NYTBS-20",
+    list: "fiction"
+  },
+  {
+    title: "Tailspin",
+    author: "Sandra Brown",
+    desc: "A pilot navigates treacherous situations when he attempts to deliver a mysterious black box to a doctor in Georgia.",
+    url: "https://www.amazon.com/Tailspin-Sandra-Brown/dp/1455572160?tag=NYTBS-20",
+    list: "fiction"
+  },
+  {
+    title: "The President Is Missing",
+    author: "Bill Clinton and James Patterson",
+    desc: "President Jonathan Duncan, a Gulf War veteran and widower, takes on adversaries at home and abroad.",
+    url: "https://www.amazon.com/President-Missing-Novel-James-Patterson/dp/0316412694?tag=NYTBS-20",
+    list: "fiction"
+  },
+  {
+    title: "Rich People Problems",
+    author: "Kevin Kwan",
+    desc: "The final book in the Crazy Rich Asians trilogy. Rivalries and jealousies arise among privileged families.",
+    url: "https://www.amazon.com/Rich-People-Problems-Kevin-Kwan/dp/0385542232?tag=NYTBS-20",
+    list: "fiction"
+  },
+  {
+    title: "Eleanor Oliphant Is Completely Fine",
+    author: "Gail Honeyman",
+    desc: "A young woman’s well-ordered life is disrupted by the I.T. guy from her office.",
+    url: "https://www.amazon.com/Eleanor-Oliphant-Completely-Fine-Novel/dp/0735220697?tag=NYTBS-20",
+    list: "fiction"
+  },
+  {
+    title: "The Rooster Bar",
+    author: "John Grisham",
+    desc: "Three students at a sleazy for-profit law school hope to expose the student-loan banker who runs it.",
+    url: "https://www.amazon.com/Rooster-Bar-John-Grisham-ebook/dp/B01N6YQAPS?tag=NYTBS-20",
+    list: "fiction"
+  },
+  {
+    title: "The Other Woman",
+    author: "Daniel Silva",
+    desc: "Gabriel Allon, the art restorer and assassin, fights the Russians to decide the fate of postwar global order.",
+    url: "https://www.amazon.com/Other-Woman-Novel-Daniel-Silva-ebook/dp/B0739Z4XJ9?tag=NYTBS-20",
+    list: "fiction"
+  },
+  {
+    title: "The Outsider",
+    author: "Stephen King",
+    desc: "A detective investigates a seemingly wholesome member of the community when an 11-year-old boy’s body is found.",
+    url: "https://www.amazon.com/Outsider-Novel-Stephen-King/dp/1501180983?tag=NYTBS-20",
+    list: "fiction"
+  },
+  {
+    title: "Less",
+    author: "Andrew Sean Greer",
+    desc: "Winner of the 2018 Pulitzer Prize in fiction. To avoid an ex-boyfriend's wedding, a failed novelist attends literary events around the world.",
+    url: "https://www.amazon.com/Less-Winner-Pulitzer-Prize-Novel/dp/031631613X?tag=NYTBS-20",
+    list: "fiction"
+  },
+  {
+    title: "Richer Than Sin",
+    author: "Meghan March",
+    desc: "Despite a family feud, a Riscoff is attracted to a seemingly single Gable.",
+    url: "https://www.amazon.com/Richer-Than-Sin-Meghan-March-ebook/dp/B07D3XSRQ4?tag=NYTBS-20",
+    list: "fiction"
+  },
+  {
+    title: "Unhinged",
+    author: "Omarosa Manigault Newman",
+    desc: "The reality TV star and former White House staffer describes her time and relationship with Donald Trump.",
+    url: "https://www.amazon.com/Unhinged-Insiders-Account-Trump-White/dp/198210970X?tag=NYTBS-20",
+    list: "nonfiction"
+  },
+  {
+    title: "The Russia Hoax",
+    author: "Gregg Jarrett",
+    desc: "The Fox News analyst makes his case against the F.B.I. investigation into collusion between the Trump campaign and Russia.",
+    url: "https://www.amazon.com/Russia-Hoax-Illicit-Hillary-Clinton/dp/0062872745?tag=NYTBS-20",
+    list: "nonfiction"
+  },
+  {
+    title: "Educated",
+    author: "Tara Westover",
+    desc: "The daughter of survivalists, who is kept out of school, educates herself enough to leave home for university.",
+    url: "https://www.amazon.com/Educated-Memoir-Tara-Westover/dp/0399590501?tag=NYTBS-20",
+    list: "nonfiction"
+  },
+  {
+    title: "Liars, Leakers and Liberals",
+    author: "The legal analyst and Fox News host argues in favor of President Trump.",
+    desc: "Jeanine Pirro",
+    url: "https://www.amazon.com/Liars-Leakers-Liberals-Anti-Trump-Conspiracy/dp/1546083421?tag=NYTBS-20",
+    list: "nonfiction"
+  },
+  {
+    title: "House of Trump, House of Putin",
+    author: "Craig Unger",
+    desc: "An investigation into the relationship between the current heads of Russia and the United States, and their potential ties to the Russian Mafia.",
+    url: "https://www.amazon.com/House-Trump-Putin-Untold-Russian/dp/152474350X?tag=NYTBS-20",
+    list: "nonfiction"
+  },
+  {
+    title: "Black Klansman",
+    author: "Ron Stallworth",
+    desc: "The first black detective of the Colorado Springs Police Department goes undercover to investigate the Ku Klux Klan.",
+    url: "https://www.amazon.com/Black-Klansman-Undercover-Investigation-Lifetime/dp/1250299055?tag=NYTBS-20",
+    list: "nonfiction"
+  },
+  {
+    title: "Everything Trump Touches Dies",
+    author: "Rick Wilson",
+    desc: "The Republican political campaign strategist gives his take on the current president and offers a way forward for conservatives.",
+    url: "https://www.amazon.com/Everything-Trump-Touches-Dies-Republican/dp/1982103124?tag=NYTBS-20",
+    list: "nonfiction"
+  },
+  {
+    title: "Sapiens",
+    author: "Yuval Noah Harari",
+    desc: "How Homo sapiens became Earth’s dominant species.",
+    url: "http://www.amazon.com/Sapiens-A-Brief-History-Humankind-ebook/dp/B00ICN066A?tag=NYTBS-20",
+    list: "nonfiction"
+  },
+  {
+    title: "Dopesick",
+    author: "Beth Macy",
+    desc: "An in-depth look at how opioid addiction affects Americans across geographic and class lines.",
+    url: "https://www.amazon.com/Dopesick-Dealers-Doctors-Company-Addicted/dp/0316551244?tag=NYTBS-20",
+    list: "nonfiction"
+  },
+  {
+    title: "Bad Blood",
+    author: "John Carreyrou",
+    desc: "The rise and fall of Theranos, the biotech startup that failed to deliver on its promise to make blood testing more efficient.",
+    url: "https://www.amazon.com/Bad-Blood-Secrets-Silicon-Startup-ebook/dp/B078VW3VM7?tag=NYTBS-20",
+    list: "nonfiction"
+  },
+  {
+    title: "The Soul of America",
+    author: "Jon Meacham",
+    desc: "The present political climate is contextualized through the lens of difficult moments in American history.",
+    url: "https://www.amazon.com/Soul-America-Battle-Better-Angels/dp/0399589813?tag=NYTBS-20",
+    list: "nonfiction"
+  },
+  {
+    title: "The Fighters",
+    author: "CJ Chivers",
+    desc: "Portraits of six American combatants who served in the wars in Afghanistan and Iraq.",
+    url: "https://www.amazon.com/Fighters-C-J-Chivers/dp/1451676646?tag=NYTBS-20",
+    list: "nonfiction"
+  },
+  {
+    title: "The Tangled Tree",
+    author: "David Quammen",
+    desc: "The lives of molecular biologists and their discoveries involving the movement of genes across species lines.",
+    url: "https://www.amazon.com/Tangled-Tree-Radical-History-Life/dp/1476776628?tag=NYTBS-20",
+    list: "nonfiction"
+  },
+  {
+    title: "Killers of The Flower Moon",
+    author: "David Grann",
+    desc: "The story of a murder spree in 1920s Oklahoma that targeted Osage Indians, whose lands contained oil.",
+    url: "https://www.amazon.com/Killers-Flower-Moon-Osage-Murders/dp/0385534248?tag=NYTBS-20",
+    list: "nonfiction"
+  },
+  {
+    title: "Kitchen Confidential",
+    author: "Anthony Bourdain",
+    desc: "A memoir-exposé of the restaurant world. Originally published in 2000.",
+    url: "http://www.amazon.com/Kitchen-Confidential-Updated-Edition-Adventures/dp/0060899220?tag=NYTBS-20",
+    list: "nonfiction"
+  }
+])
