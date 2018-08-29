8e8d8bd0077b1de6fe1ba94480affe77c0551d04
Kelsey Fecho <kfecho30@gmail.com>
2018-08-29 18:08:12 -0400

    Change actions to pull from internal rails api

--- client/src/actions/bookActions.js
+++ client/src/actions/bookActions.js
@@ -1,11 +1,11 @@
-const API_KEY =`${process.env.REACT_APP_NYT_API_KEY}`
+/** Async Requests**/
 
 export function fetchFiction(){
   return (dispatch) => {
     dispatch({type: 'LOADING'});
-    return fetch(`https://api.nytimes.com/svc/books/v3/lists.json?list=combined-print-and-e-book-fiction&api-key=${API_KEY}`)
+    return fetch('/api/book?q=fiction')
      .then(res => {return res.json()})
-     .then(({results}) => {dispatch({type: 'FETCH_FICTION', payload: results})
+     .then(results => {dispatch({type: 'FETCH_FICTION', payload: results})
    })
   }
 }
@@ -13,9 +13,9 @@ export function fetchFiction(){
 export function fetchNonfiction(){
   return (dispatch) => {
     dispatch({type: 'LOADING'});
-    return fetch(`https://api.nytimes.com/svc/books/v3/lists.json?list=combined-print-and-e-book-nonfiction&api-key=${API_KEY}`)
+    return fetch('/api/book?q=nonfiction')
      .then(res => {return res.json()})
-     .then(({results}) => {dispatch({type: 'FETCH_NONFICTION', payload: results})
+     .then(results => {dispatch({type: 'FETCH_NONFICTION', payload: results})
    })
   }
 }
