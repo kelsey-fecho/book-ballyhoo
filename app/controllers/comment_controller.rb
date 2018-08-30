d00050b6c99cfad9efbc1335ea52aa2c216a7bf2
Kelsey Fecho <kfecho30@gmail.com>
2018-08-30 10:23:46 -0400

    Add rails controller action to persist new comments to the database

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -1,5 +1,17 @@
 class CommentController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     render status: 200, json: Comment.all
+  end
+
+  def create
+    comment = Comment.create(comment_params)
+    render :json => Comment.all
+  end
+
+  private
+  def comment_params
+    params.require(:data).permit(:comment, :author)
   end
 end
